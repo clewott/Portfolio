c913ca96f1d507a366f271e385d5a3e9c857002c
crotten <wildrunts@gmail.com>
2014-10-16 11:55:04 -0400

    added a  mouseover effect

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
+$(document).ready( function() {
 
-function printDiv() {
-     window.print();
-}
+    function printDiv() {
+         window.print();
+    }
+
+    $('.projectImage').on("mouseover", function() {
+      $(this).addClass("coolEffect");
+      $(this).next('.hiddenH3').fadeIn(500);
+      });
+    $('.projectImage').on("mouseout", function() {
+      $(this).removeClass("coolEffect");
+      $(this).next('.hiddenH3').fadeOut(500);
+      });
+
+});
